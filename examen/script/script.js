80e83db6c5441913af3680977cd1aa3d3b19e031
davidorozco69 <100798924+davidorozco69@users.noreply.github.com>
2022-03-02 16:35:49 -0500

    Add files via upload

---
+++ examen/script/script.js
@@ -0,0 +1,17 @@
+const he = 30000
+const hn = 20000
+let canH, horas, total
+function leerdatos(){
+    salirio = Number(prompt(' ¿cuantas horas tranajo? '))
+
+}
+leerdatos()
+calcular(salario)
+
+function calcular(horas){
+    if(horas <= 1 && horas <= 40){
+    }
+    else if( horas<40)
+    he = horas - 40
+    salario =(canH * )+(ex * hn)
+}
